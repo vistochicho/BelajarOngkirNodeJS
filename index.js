08de425c5f8456bcdf4c7c81fe44764ffd4f3fe3
Vistheu <49154926+vistochicho@users.noreply.github.com>
2023-07-13 23:14:50 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Butuh express, makanya dipanggil
+const express = require("express")
+const path = require("path")
+app = express()
+
+
+//nge set engine viewnya pake ejs
+app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs");
+app.use('/assets', express.static(__dirname + '/public/assets'))
+
+// ngeroute ke halaman utama
+app.get("/", function (req, res) {
+    res.render("pages/index")  
+})
+
+// buat servernya
+app.listen(8080, function() {
+    console.log("Server is running bro")
+})
